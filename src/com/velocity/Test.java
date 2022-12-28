d9d39d78acc017f037ec6401603ed38b8a527772
SShifa <sifasasture@gmail.com>
2022-12-28 19:18:53 +0530

    Swapping number

---
+++ src/com/velocity/Test.java
@@ -0,0 +1,21 @@
+package com.velocity;
+
+public class Test {
+
+	public static void main(String[] args) {
+		
+		int a=20;
+		int b=30;
+		int temp;
+		System.out.println("Before swapping"+" "+a+" "+b);
+		
+		temp=a;
+		a=b;
+		b=temp;
+		
+		System.out.println("after swapping"+" "+a+" "+b);
+		
+
+	}
+
+}
